516866742cfa3912b6442923b4b90a0d281d1fa5
Solcan Moise <solcan_moise2014@yahoo.com>
2019-05-19 13:35:19 +0300

    Communicating between scopes

--- app/controllers/reusableCtrl.js
+++ app/controllers/reusableCtrl.js
@@ -1,12 +1,24 @@
 angular.module("controllers")
-.controller("reusableCtrl", function($scope){
+.controller("reusableCtrl", function($scope, $rootScope){
+    
+    // registers a handler function that is invoked when the specified event 
+    // is received by the scope.
+    $scope.$on("zipCodeUpdated", function(event, args){
+        console.log(args);
+        $scope[args.type] = args.zipCode;
+    });
     
     $scope.setAddress = function(type, zip){
-        console.log("Type: " + type + " and ZIP: " + zip);
+        // sends an event fro the current scope down to all of the child scopes
+        $rootScope.$broadcast("zipCodeUpdated", {
+            type: type, zipCode: zip
+        });
     }
     
     $scope.copyAddress = function(){
-        $scope.shippingZip = $scope.billingZip;
+        $scope.zip = $scope.billingZip;
     }
+    
+    // $emit(name, args) - emit an event from the current scope up to the root scope.
     
 });
