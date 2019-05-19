340f64f6dffd42b1506b5859aced3cd86b477718
Solcan Moise <solcan_moise2014@yahoo.com>
2019-05-19 09:29:51 +0300

    Setting Up the Scope

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,4 +1,13 @@
 angular.module("controllers", [])
 .controller("mainCtrl", function($scope){
     
+    $scope.city = 'London';
+    
+    $scope.getCountry = function(city){
+        switch(city){
+            case "London": return "UK";
+            case "New York": return "USA";
+        }
+    }
+    
 });
