de9f2525a391170e7c1ba4e48a01546c22bcedea
Solcan Moise <solcan_moise2014@yahoo.com>
2019-05-19 09:39:13 +0300

    Modifying the Scope

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,6 +1,8 @@
 angular.module("controllers", [])
 .controller("mainCtrl", function($scope){
     
+    $scope.cities = ["London", "New York", "Paris"];
+    
     $scope.city = 'London';
     
     $scope.getCountry = function(city){
