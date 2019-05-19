38e895021ff09ccb658a065cf112e1c45ab055d4
Solcan Moise <solcan_moise2014@yahoo.com>
2019-05-19 13:54:06 +0300

    Using a Service to mediate scope events

--- app/controllers/reusableCtrl.js
+++ app/controllers/reusableCtrl.js
@@ -1,24 +1,16 @@
 angular.module("controllers")
-.controller("reusableCtrl", function($scope, $rootScope){
+.controller("reusableCtrl", ["$scope", "ZipCodes", function($scope, zipCodes){
     
-    // registers a handler function that is invoked when the specified event 
-    // is received by the scope.
     $scope.$on("zipCodeUpdated", function(event, args){
-        console.log(args);
         $scope[args.type] = args.zipCode;
     });
     
     $scope.setAddress = function(type, zip){
-        // sends an event fro the current scope down to all of the child scopes
-        $rootScope.$broadcast("zipCodeUpdated", {
-            type: type, zipCode: zip
-        });
+        zipCodes.setZipCode(type, zip);
     }
     
     $scope.copyAddress = function(){
         $scope.zip = $scope.billingZip;
     }
     
-    // $emit(name, args) - emit an event from the current scope up to the root scope.
-    
-});
+}]);
